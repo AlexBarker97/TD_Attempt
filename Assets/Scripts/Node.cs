20eb9acf40780c2e0edd09c20365da2537b12ae6
Alex Barker <alexbarker31@gmail.com>
2023-01-28 22:28:05 +0000

    0.15

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,6 +6,11 @@ public class Node : MonoBehaviour
 	public Color hoverColor;
 	private Renderer rend;
 	private Color startColor;
+	public GameObject Turret1Ghost;
+	public GameObject Turret1Real;
+	public bool gndTurret = false;
+	private GameObject Old;
+	
 
 	void Start()
 	{
@@ -15,12 +20,36 @@ public class Node : MonoBehaviour
 
 	void OnMouseEnter()
 	{
-		rend.material.color = Color.white;
+		if (rend.gameObject.name == "GndNode(Clone)")
+        {
+			Instantiate(Turret1Ghost, rend.gameObject.transform.position + new Vector3(0f, 1.5f, 0f), Quaternion.Euler(0, 0, 0));
+			Old = rend.gameObject;
+			if(Old.name == "GndNode(Clone)")
+            {
+				Destroy(GameObject.Find("Turret1ghost(Clone)"));
+			}
+		}
+	}
+
+	void OnMouseOver()
+	{
+		Debug.Log(GameObject.Find("Shop").GetComponent<Shop>().state);
+		Debug.Log(rend.gameObject.name);
+		if ((GameObject.Find("Shop").GetComponent<Shop>().state == 2) & ((rend.gameObject.name == "GndNode(Clone)")|(rend.gameObject.name == "Cylinder")))
+        {
+			if (Input.GetMouseButtonDown(1))
+			{
+				GameObject.Find("Shop").GetComponent<Shop>().state = 3;
+			}
+			if (Input.GetMouseButtonDown(0))
+			{
+				GameObject.Find("Shop").GetComponent<Shop>().state = 4;
+			}
+		}
 	}
 
 	void OnMouseExit()
 	{
-		rend.material.color = startColor;
 	}
 
 }
--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -6,7 +6,12 @@ public class Shop : MonoBehaviour
 {
     public int cash;
     public Text cashRemainingText;
-    public GameObject Turret1;
+    public GameObject Turret1Ghost;
+    public GameObject Turret1Real;
+    public int T1Cost = 100;
+    public int state = 0;
+    public string turret = "nil";   //will spawn based on turret selected
+    private Vector3 pos;
 
     void Start()
     {
@@ -16,12 +21,48 @@ public class Shop : MonoBehaviour
     public void PurchaseTurret1()
     {
         Debug.Log("Purchase Turret 1");
-        Instantiate(Turret1, new Vector3(0f,0f,0f), Quaternion.Euler(0, 0, 0));
-        cash = cash - 100;
+        if (state == 0)
+        {
+            state = 1;
+        }
     }
 
-    void Update()
+void Update()
     {
+        //Debug.Log(state);
         cashRemainingText.text = cash.ToString();
+        switch (state)
+        {
+            case 0:
+                //calculated in PurchaseTurret1()
+                break;
+            case 1:
+                if(cash>=T1Cost)
+                {
+                    Instantiate(Turret1Ghost, new Vector3(0f, 0f, 0f), Quaternion.Euler(0, 0, 0));
+                    state = 2;
+                }
+                else
+                {
+                    state = 0;
+                    //playsound wah
+                }
+                break;
+            case 2:
+                    //calculated in Node.cs
+                break;
+            case 3:
+                Destroy(GameObject.Find("Turret1ghost(Clone)"));
+                state = 0;
+                break;
+            case 4:
+                pos = GameObject.Find("Turret1ghost(Clone)").transform.position;
+                Instantiate(Turret1Real, pos, Quaternion.Euler(0, 0, 0));
+                Destroy(GameObject.Find("Turret1ghost(Clone)"));
+                cash = cash - T1Cost;
+                state = 0;
+                //System.Threading.Thread.Sleep(10000);
+                break;
+        }
     }
 }
